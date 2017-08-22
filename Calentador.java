b927148998c9d1bbc936fab7726aa73fc23702d9
daniel vallejo <daniel_vallejoquiin@hotmail.com>
2017-08-22 18:45:18 -0500

    Intercambio inicial de proyecto

---
+++ Calentador.java
@@ -0,0 +1,29 @@
+public class Calentador
+{
+    private int temperatura;
+    
+    public Calentador()
+    {
+        temperatura = 15;
+    }
+    
+    public void calentar()
+    {
+        temperatura = temperatura + 5;
+    }
+    
+    public void enfria()
+    {
+        temperatura = temperatura -5;
+    }
+    
+    public void muestra()
+    {
+        System.out.println(temperatura);
+    }
+    
+    public void restaura()
+    {
+        temperatura = 15;
+    }
+}
