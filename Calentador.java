ed776ae96e151d322ae8b5655133a51a73d316fc
daniel vallejo <daniel_vallejoquiin@hotmail.com>
2017-08-31 19:37:41 -0500

    configure el programa para corregir errores de sobre paso en enfriamiento y calentamiento, asi como la ducumentacion de los metodos

--- Calentador.java
+++ Calentador.java
@@ -2,16 +2,28 @@ public class Calentador
 {
     private int temperatura;
     
+    /**
+     * Constructor que inicializa la temperatura en 15 del objeto
+     */
     public Calentador()
     {
         temperatura = 15;
     }
     
+    /**
+     * Medoto que sube a 3 unidades la temperatura pero no mayor a 30
+     */
     public void calentar()
     {
         if (temperatura<30)
         {
-          temperatura = temperatura + 3;  
+          
+            temperatura = temperatura + 3;
+            if(temperatura<30)
+            {
+                temperatura = temperatura -3;
+                System.out.println("No se puede subir mas la temperatura");
+            }
         }
         else
         {
@@ -19,23 +31,38 @@ public class Calentador
         }
     }
     
-    public void enfria()
+    /**
+     * Este metodo enfria pero con un limite de -15, va restando de 3 unidades
+     */public void enfria()
     {
-        if (-10<=temperatura)
-        {
-            temperatura = temperatura -3;
-        }
-        else
-        {
-            System.out.println("No se puede bajar mas la temperatura");
-        }
+        
+            if (temperatura>-10)
+               {
+                temperatura = temperatura -3;
+                if(temperatura<=-10)
+                {
+                    temperatura = temperatura +3;
+                    System.out.println("No se puede bajar mas");
+                }
+               }
+            else
+               {
+                System.out.println("No se puede bajar mas la temperatura");
+               }
+        
     }
     
+    /**
+     * Metodo que imprime la temperatura actual del calentador
+     */
     public void muestra()
     {
         System.out.println(temperatura);
     }
     
+    /**
+     * Metodo que restaura a la configuracion inicial el calentador
+     */
     public void restaura()
     {
         temperatura = 15;
